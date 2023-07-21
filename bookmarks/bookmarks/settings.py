bf9f571ed1fed48330cf4f49a6de951afc0bf5d2
Julio Gazoli <julio_gazoli@hotmail.com>
2023-07-21 18:28:30 -0300

    creating a profile - social authentication

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -156,3 +156,17 @@ SOCIAL_AUTH_TWITTER_SECRET = '' # Twitter API Secret
 # Google
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '' # Google Client ID
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '' # Google Client Secret
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+
+]
